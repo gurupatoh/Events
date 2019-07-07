4617128b5d9f04af33be1b7ef3ffe0e665c50a6f
gurupatoh <patrickn0023@gmail.com>
2019-07-07 13:39:04 -0700

    added checkout added upload images in Ticket controller added  gouldeman cart

---
+++ config/notifications.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'notifications' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Notification Renderer
+        |--------------------------------------------------------------------------
+        |
+        | Specify which renderer to use to render the notifications
+        |
+        | Supported: "native", "toastr", "sweetalert", "bootbox"
+        |
+        */
+        'renderer' => 'native'
+
+    ]
+
+];
